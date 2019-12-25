36539435a1cab08f66832a4241e725c45a789044
omkar <omkarbhiwande@gmail.com>
2019-12-26 00:24:15 +0530

    first commit

---
+++ src/pages/signup.js
@@ -0,0 +1,11 @@
+class Signup extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Signup page</h1>
+            </div>
+        )
+    }
+}
+
+export default Signup
